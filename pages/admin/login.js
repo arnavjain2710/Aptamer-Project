9b852e4a322390fa2285806b3d36614896e1d33c
Siddhesh <siddheshwaje123456@gmail.com>
2024-03-20 00:49:10 +0530

    css

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -1,15 +1,9 @@
-// let jwt = localStorage.getItem("jwt");
-// if (jwt != null) {
-//   window.location.href = "./admin.html";
-// }
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("my-form");
 
   form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    // Get form data
+    event.preventDefault();
     const formData = {
       username: document.getElementById("username").value,
       password: document.getElementById("password").value,
@@ -28,33 +22,5 @@ document.addEventListener("DOMContentLoaded", function () {
     );
 
     console.log(xhttp.responseText);
-
-
-
-    // xhttp.onreadystatechange = function () {
-    //     if (this.readyState == 4) {
-    //       const objects = JSON.parse(this.responseText);
-    //       console.log(objects);
-    //       if (objects['status'] == 'ok') {
-    //         localStorage.setItem("jwt", objects['accessToken']);
-    //         Swal.fire({
-    //           text: objects['message'],
-    //           icon: 'success',
-    //           confirmButtonText: 'OK'
-    //         }).then((result) => {
-    //           if (result.isConfirmed) {
-    //             window.location.href = '../../index.html';
-    //           }
-    //         });
-    //       } else {
-    //         Swal.fire({
-    //           text: objects['message'],
-    //           icon: 'error',
-    //           confirmButtonText: 'OK'
-    //         });
-    //       }
-    //     }
-    //   };
-    //  
   });
 });
