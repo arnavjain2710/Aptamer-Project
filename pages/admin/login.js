b607dc47bc9062d375efb53ecb1cd7c4e07a832c
TheNetherWatcher <117659673+TheNetherWatcher@users.noreply.github.com>
2024-03-26 19:39:11 +0530

    added redirections and login changes

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -2,25 +2,38 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("my-form");
 
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    const formData = {
-      username: document.getElementById("username").value,
-      password: document.getElementById("password").value,
-    };
+      form.addEventListener('submit', function(event) {
+          event.preventDefault(); // Prevent the default form submission behavior
+          // Get form data
+          const formData = {
+              email: document.getElementById("username").value,
+              password: document.getElementById("password").value,
+          };
 
-    console.log(formData);
-
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "https://aptabase.shuttleapp.rs/auth/login");
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhttp.send(
-      JSON.stringify({
-        email: username,
-        password: password,
-      })
-    );
-
-    console.log(xhttp.responseText);
-  });
+          // Make the API call
+          try {
+            fetch('https://aptabase.shuttleapp.rs/v1/auth/login', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(formData)
+          })
+              .then(response => {
+                  if(response.status == 200){
+                      console.log(response);``
+                      const token = response.headers.get('token');
+                      console.log(token);
+                      localStorage.setItem('authtoken', token);
+                      window.open("http://127.0.0.1:5501/pages/redirect/redirect_links.html", "_blank")
+                  } else {
+                      window.alert("Wrong creadentials")
+                  }
+              });
+          } catch (error) {
+            console.log(error);
+            window.alert("Wrong credentials")
+          }
+          
+      });
 });
